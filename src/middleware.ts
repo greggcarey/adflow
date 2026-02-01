be8ebc28558f87ca842423b7a5e904c9f3cbe2b8
Gregg Carey <greggcarey@MacBook-Pro-7.local>
2026-01-31 23:15:52 -0500

    Exclude debug-env from middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,6 +37,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.png$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|api/debug-env|.*\\.png$).*)",
   ],
 };
